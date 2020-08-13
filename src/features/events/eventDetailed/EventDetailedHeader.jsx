7d4b88b524226b2c071c1e613c56f436564fd2f1
Asger <asge4900@edu.campusvejle.dk>
2020-08-13 14:18:40 +0200

    End of section 6

--- src/features/events/eventDetailed/EventDetailedHeader.jsx
+++ src/features/events/eventDetailed/EventDetailedHeader.jsx
@@ -3,23 +3,27 @@ import { Segment, Image, Item, Header, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 const eventImageStyle = {
-    filter: 'brightness(30%)'
+  filter: "brightness(30%)",
 };
 
 const eventImageTextStyle = {
-    position: 'absolute',
-    bottom: '5%',
-    left: '5%',
-    width: '100%',
-    height: 'auto',
-    color: 'white'
+  position: "absolute",
+  bottom: "5%",
+  left: "5%",
+  width: "100%",
+  height: "auto",
+  color: "white",
 };
 
-export default function EventDetailedHeader() {
+export default function EventDetailedHeader({ event }) {
   return (
     <Segment.Group>
       <Segment basic attached='top' style={{ padding: "0" }}>
-        <Image src={`/assets/categoryImages/drinks.jpg`} fluid style={eventImageStyle} />
+        <Image
+          src={`/assets/categoryImages/${event.category}.jpg`}
+          fluid
+          style={eventImageStyle}
+        />
 
         <Segment basic style={eventImageTextStyle}>
           <Item.Group>
@@ -27,12 +31,12 @@ export default function EventDetailedHeader() {
               <Item.Content>
                 <Header
                   size='huge'
-                  content='Event Title'
+                  content={event.title}
                   style={{ color: "white" }}
                 />
-                <p>Event Date</p>
+                <p>{event.date}</p>
                 <p>
-                  Hosted by <strong>Bob</strong>
+                  Hosted by <strong>{event.hostedBy}</strong>
                 </p>
               </Item.Content>
             </Item>
@@ -44,7 +48,12 @@ export default function EventDetailedHeader() {
         <Button>Cancel My Place</Button>
         <Button color='teal'>JOIN THIS EVENT</Button>
 
-        <Button as={Link} to={`/manage/`} color='orange' floated='right'>
+        <Button
+          as={Link}
+          to={`/manage/${event.id}`}
+          color='orange'
+          floated='right'
+        >
           Manage Event
         </Button>
       </Segment>
---
+++ src/features/sandbox/Sandbox.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Button } from "semantic-ui-react";
+import { decrement, increment } from "./testReducer";
+
+export default function Sandbox() {
+  const dispath = useDispatch();
+  const data = useSelector((state) => state.test.data);
+
+  return (
+    <>
+      <h1>Testing 123</h1>
+      <h3>The data is: {data} </h3>
+      <Button
+        onClick={() => dispath(increment(20))}
+        content='Increment'
+        color='green'
+      />
+      <Button
+        onClick={() => dispath(decrement(10))}
+        content='Decrement'
+        color='red'
+      />
+    </>
+  );
+}
